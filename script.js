718b33815465312632aacdb880e7796e1f413334
abhi-develope <prajapatiabhishek320@gmail.com>
2024-04-30 23:44:45 +0530

    fix some animation

--- script.js
+++ script.js
@@ -61,8 +61,9 @@ window.onscroll = ()=>{
 
        ScrollReveal().reveal('.home-content, .heading', {origin: 'top'})
        ScrollReveal().reveal('.home-img, .services-container,  .portfolio-box, .contact form', {origin: 'bottom'})
-       ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'})
-       ScrollReveal().reveal('.home-content p, .about-content, .mySkill', {origin: 'right'})
+       ScrollReveal().reveal('.home-content h1, .about-content, .about-img', {origin: 'left'})
+       ScrollReveal().reveal('.home-content p,  .mySkill', {origin: 'right'})
+       
 
 
        /* typed js */
